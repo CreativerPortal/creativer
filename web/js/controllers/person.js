525c883b4b9909a8c8ff0e071810403b406ee83d
root <root@creativer.by>
2015-09-12 11:21:08 +0000

    feedback

--- web/js/controllers/person.js
+++ web/js/controllers/person.js
@@ -226,9 +226,10 @@ angular.module('app.ctr.person', ['service.personal', 'angularFileUpload', 'serv
 
     $scope.sendFeedBack = function(){
         $scope.loader = true;
-        //personalService.sendFeedBack({nick: $scope.nick, telephone: $scope.telephone, message: $scope.message}).success(function (data) {
-        //    $scope.sent_letter = true;
-        //});
+        personalService.sendFeedBack({nick: $scope.nick, telephone: $scope.telephone, message: $scope.message}).success(function (data) {
+            $scope.sent_letter = true;
+            $scope.loader = false;
+        });
     }
 
     $scope.$on('$routeChangeStart', function(next, current) {
