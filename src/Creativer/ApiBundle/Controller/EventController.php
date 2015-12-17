cc8cc78466d3baa3285f2e81e6301fb25fb123e9
slaq777 <slaq@tut.by>
2015-12-17 07:44:03 +0300

    change the time in EventController

--- src/Creativer/ApiBundle/Controller/EventController.php
+++ src/Creativer/ApiBundle/Controller/EventController.php
@@ -92,10 +92,10 @@ class EventController extends Controller
             $plus_period = 15 - (31 - (int)$start_month);
             $cr = clone $current_date;
             $end_month = $cr->modify('last day of this month')->modify('+ '.$plus_period.' day')->setTime(23, 59, 59)->format('Y/m/d H:i:s');
-            $start_month = $current_date->setTime(00, 00, 00)->format('Y/m/d H:i:s');
+            $start_month = $current_date->setTime(01, 00, 00)->format('Y/m/d H:i:s');
         }else{
             $current_date = $date = new \DateTime($other_date);
-            $start_month = $current_date->setTime(00, 00, 00)->format('Y/m/d H:i:s');
+            $start_month = $current_date->setTime(01, 00, 00)->format('Y/m/d H:i:s');
             $end_month = $date->modify('last day of this month')->setTime(23, 59, 59)->format('Y/m/d H:i:s');
         }
 
